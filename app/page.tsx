beb1ca35c68d601a83fdf4e2b363f297b0b750e3
Ewooral <boahen.cos@gmail.com>
2023-12-08 12:48:42 +0000

    chore: initial

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
      <section>
-      <h1>Hello, World!</h1>
+      <h1>EGANOW MULTI STEPPER FORM</h1>
      </section>
     </main>
   )
